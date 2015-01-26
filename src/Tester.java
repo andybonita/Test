570e7059c82eeda97661173a300e9305fe48ab58
Andy <psgman90@hotmail.fr>
2015-01-26 23:59:14 +0100

    test

---
+++ src/Tester.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 26/01/2015.
+ */
+public class Tester {
+    public int nbr;
+    public String state;
+
+    public Tester( int n, String st){
+        this.nbr=n;
+        this.state=st;
+    }
+}
